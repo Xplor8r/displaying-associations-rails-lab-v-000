370bc6e8dba28884f26ce12cd8e8216f31eb880c
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-01 17:47:40 +0000

    automatically backed up by learn

--- db/migrate/20160108015311_create_songs.rb
+++ db/migrate/20160108015311_create_songs.rb
@@ -3,7 +3,7 @@ class CreateSongs < ActiveRecord::Migration
     create_table :songs do |t|
       t.string :title
       t.integer :artist_id
-      
+
       t.timestamps null: false
     end
   end
